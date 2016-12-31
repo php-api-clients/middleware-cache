bb612f0da69a07b1f7587fbf0ea19af17238c2ef
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-31 20:07:07 +0100

    Default TTL of 0

--- src/StrategyInterface.php
+++ src/StrategyInterface.php
@@ -7,7 +7,35 @@ use Psr\Http\Message\ResponseInterface;
 
 interface StrategyInterface
 {
+    const DEFAULT_TTL = 0;
+
+    /**
+     * Check whether to store to cache based on the request
+     *
+     * @param RequestInterface $request
+     * @return bool
+     */
     public function preCheck(RequestInterface $request): bool;
+
+    /**
+     * Check whether to store to cache based on the response
+     *
+     * @param ResponseInterface $response
+     * @return bool
+     */
     public function postCheck(ResponseInterface $response): bool;
-    public function determineTtl(RequestInterface $request, ResponseInterface $response): int;
+
+    /**
+     * Determine what the TTL for the cache entry should be based on the request and response
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param int $default
+     * @return int
+     */
+    public function determineTtl(
+        RequestInterface $request,
+        ResponseInterface $response,
+        int $default = self::DEFAULT_TTL
+    ): int;
 }
